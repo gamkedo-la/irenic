dc00e4f70cff231a42f630b03d696125bea8a5f5
Caspar Dunant <spadxiii@gmail.com>
2016-10-22 08:03:04 +0200

    Changed Tile class

--- js/tile.js
+++ js/tile.js
@@ -13,60 +13,60 @@ var Tile = function(index) {
   this.col = 0;
   this.spriteX = spriteCol * TILE_WIDTH;
   this.spriteY = spriteRow * TILE_HEIGHT;
-};
 
-// @todo do something with first positioning
-// @todo do something with animated shuffling
-Tile.prototype.placeAtIndex = function(index) {
-  this.col = Grid.indexToCol(index, GRID_COLS);
-  this.row = Grid.indexToRow(index, GRID_COLS);
-
-  this.x = this.col * (TILE_WIDTH + TILE_GAP);
-  this.y = this.row * (TILE_HEIGHT + TILE_GAP);
-
-  return this;
-};
-
-Tile.prototype.reset = function() {
-  this.active = false;
-  return this;
-};
-
-Tile.prototype.update = function(time) {
-  this.readyToRemove = this.readyToRemove || (this.matching > 0 && this.matching < time);
-};
-
-Tile.prototype.draw = function() {
-  if (this.readyToRemove) {
-    return;
-  }
-
-  var image = Images.tiles;
-  if (this.active || this.matching) {
-    image = Images.tiles_active;
-  }
-  else if (this.hover()) {
-    image = Images.tiles_hover;
-  }
-  gameContext.drawImage(image, this.spriteX, this.spriteY, TILE_WIDTH, TILE_HEIGHT, this.x, this.y, TILE_WIDTH, TILE_HEIGHT);
-};
-
-Tile.prototype.bounds = function() {
-  return {
-    top: this.y,
-    bottom: this.y + TILE_HEIGHT,
-    left: this.x,
-    right: this.x + TILE_WIDTH
+  // @todo do something with first positioning
+  // @todo do something with animated shuffling
+  this.placeAtIndex = function(index) {
+    this.col = Grid.indexToCol(index, GRID_COLS);
+    this.row = Grid.indexToRow(index, GRID_COLS);
+  
+    this.x = this.col * (TILE_WIDTH + TILE_GAP);
+    this.y = this.row * (TILE_HEIGHT + TILE_GAP);
+  
+    return this;
   };
-};
-
-Tile.prototype.hover = function() {
-  var b = this.bounds();
-  return b.left < mouse.x && mouse.x < b.right &&
-    b.top < mouse.y && mouse.y < b.bottom;
-};
-
-Tile.prototype.match = function() {
-  // @todo removal particle/animation
-  this.matching = countdownTime(TIMEOUT_IS_MATCH);
+  
+  this.reset = function() {
+    this.active = false;
+    return this;
+  };
+  
+  this.update = function(time) {
+    this.readyToRemove = this.readyToRemove || (this.matching > 0 && this.matching < time);
+  };
+  
+  this.draw = function() {
+    if (this.readyToRemove) {
+      return;
+    }
+  
+    var image = Images.tiles;
+    if (this.active || this.matching) {
+      image = Images.tiles_active;
+    }
+    else if (this.hover()) {
+      image = Images.tiles_hover;
+    }
+    gameContext.drawImage(image, this.spriteX, this.spriteY, TILE_WIDTH, TILE_HEIGHT, this.x, this.y, TILE_WIDTH, TILE_HEIGHT);
+  };
+  
+  this.bounds = function() {
+    return {
+      top: this.y,
+      bottom: this.y + TILE_HEIGHT,
+      left: this.x,
+      right: this.x + TILE_WIDTH
+    };
+  };
+  
+  this.hover = function() {
+    var b = this.bounds();
+    return b.left < mouse.x && mouse.x < b.right &&
+      b.top < mouse.y && mouse.y < b.bottom;
+  };
+  
+  this.match = function() {
+    // @todo removal particle/animation
+    this.matching = countdownTime(TIMEOUT_IS_MATCH);
+  };
 };
